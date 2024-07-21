b817b72240a66009ca5d0fd7818176514cb4e136
EC2 Default User <ec2-user@ip-172-31-14-221.ap-northeast-1.compute.internal>
2024-07-21 16:30:17 +0000

    userとbookのshow作成完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
   def show
+    @book=Book.new
+    @books=Book.find(params[:id])
+    @user=@books.user
   end
 
   def index
@@ -11,7 +14,7 @@ class BooksController < ApplicationController
     @book=Book.new(book_params)
     @book.user_id=current_user.id
     @book.save
-    redirect_to root_path
+    redirect_to book_path(@book.id)
   end
 
   def edit
